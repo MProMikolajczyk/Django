56eb2dd300d5ec5573114b3e49b254fd9699991f
mmpro <marekmikolajczyk87@gmail.com>
2019-02-11 16:57:42 +0100

    from2

--- src/products/forms.py
+++ src/products/forms.py
@@ -2,6 +2,20 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label="",
+                            widget=forms.TextInput(attrs={'placeholder': 'your title '}))
+    email = forms.EmailField()
+    description = forms.CharField(required=False, widget=forms.Textarea(
+        attrs={
+            'placeholder': 'your title ',
+            'class':'new-class-name two',
+            'id':'my-id-for-textarea',
+            'row':120,
+            'cols':120
+        }
+    ))
+    price = forms.DecimalField(initial=199.99)
+
     class Meta:
         model = Product
         fields = [
@@ -9,7 +23,18 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+    def clean_title(self,*args,**kwargs):
+        title = self.cleaned_data.get("title")
+        if "CFE" in title:
+            return title
+        else:
+            raise forms.ValidationError('This is not valid titile')
 
+    def clean_email(self,*args,**kwargs):
+        email = self.cleaned_data.get("email")
+        if not email.endswith('edu'):
+            raise forms.ValidationError('This is not valid email')
+        return email
 
 class RawProductForm(forms.Form):
     title = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder':'your title '}))
