6b3283670c83a65af487c346f815c5e5bc4d009f
mmpro <marekmikolajczyk87@gmail.com>
2019-02-11 16:31:22 +0100

    forms django

--- src/pages/views.py
+++ src/pages/views.py
@@ -14,8 +14,14 @@ def contact_view(request,*args,**kwargs):
     return render(request, "contact.html", {})
 
 def About_view(request,*args,**kwargs):
+    my_context = {
+        "title": "This is about us",
+        "my_numer": 123,
+        "my_list":[123,12334,343,"ABC"],
+        "my_html":"<h1>Hello wolrd</h1>"
+    }
     #return HttpResponse('<h1>About Page</h1>')
-    return render(request, "about.html", {})
+    return render(request, "about.html", my_context)
 
 def Social_view(request,*args,**kwargs):
     return HttpResponse('<h1>Social Page</h1>')
---
+++ src/products/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Product
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            'title',
+            'description',
+            'price'
+        ]
+
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder':'your title '}))
+    description = forms.CharField(required=False, widget=forms.Textarea(
+        attrs={
+            'placeholder': 'your title ',
+            'class':'new-class-name two',
+            'id':'my-id-for-textarea',
+            'row':120,
+            'cols':120
+        }
+    ))
+    price = forms.DecimalField(initial=199.99)
